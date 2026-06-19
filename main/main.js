c066f23efd5cb0b17bf480163bd57323a6510d49
Tester Inet Doc <tester.inet.doc@gmail.com>
2026-06-19 10:22:32 +0700

    test

--- main/main.js
+++ main/main.js
@@ -20,12 +20,12 @@ import { api11 } from '../api/api11.js';
 //============================================================================
 
 export default function () {    //เรียกใช้ API ใน export default function
-  //response = api1()
+  response = api1()
   //response = api2()
   //response = api3()
   //response = api4()
   //response = api5()
-  response = api6()
+  //response = api6()
   //response = api7()
   //response = api8()
   //response = api9()
